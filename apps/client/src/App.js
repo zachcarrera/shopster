50617b08786c0b994ad726869f658daf4e96bf5d
Jsalamone12 <joseph.salamone12@gmail.com>
2023-03-28 13:19:44 -0500

    cart/checkout/fail/success

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { DashboardView } from "./views";
+import { DashboardView, ShoppingCart, Checkout, Success, Fail } from "./views";
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<DashboardView />} />
+                <Route path="/shoppingcart" element={<ShoppingCart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/success" element={<Success />} />
+                <Route path="/fail" element={<Fail />} />
             </Routes>
         </div>
     );
