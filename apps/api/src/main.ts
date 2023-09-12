f7da58d55d8cb15312909dc5cedc08d269cbd090
Zach Carrera <zachcarrera@gmail.com>
2023-09-12 13:31:26 -0700

    refactor: update main.ts import to routes/index to a ts file and format file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config.js';
-import './configs/index.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config.js";
+import "./configs/index.js";
 
-import { productRouter, checkoutRouter } from './routes/index.js';
-import * as middleware from './middleware/index.js';
+import { productRouter, checkoutRouter } from "./routes/index";
+import * as middleware from "./middleware/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/products', productRouter);
-app.use('/api/checkouts', checkoutRouter);
+app.use("/api/products", productRouter);
+app.use("/api/checkouts", checkoutRouter);
 app.use(middleware.errorLogger);
 app.use(middleware.errorHandler);
 
